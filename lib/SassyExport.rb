136d9b212f87c68ecbd8b73e2f6024c462fa81db
Ezekiel Gabrielse <ezekg@yahoo.com>
2014-07-29 11:06:06 -0500

    add important flag to comments

--- lib/SassyExport.rb
+++ lib/SassyExport.rb
@@ -7,8 +7,8 @@ Compass::Frameworks.register('SassyExport', :path => extension_path)
 # Version is a number. If a version contains alphas, it will be created as a prerelease version
 # Date is in the form of YYYY-MM-DD
 module SassyExport
-  VERSION = "1.3.3"
-  DATE = "2014-07-24"
+  VERSION = "1.3.4"
+  DATE = "2014-07-29"
 end
 
 # SassyExport : convert passed map to json and write to path/to/filename.json
